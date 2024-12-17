6db63fcbbce8ae629aedfd002d32984ec2ed662f
Munish Rana <munishmr2007@gmail.com>
2024-12-17 16:36:59 +0530

    Added url for events for laravel application 4

--- app/models/token.py
+++ app/models/token.py
@@ -7,7 +7,7 @@ class Token(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"))
-    token = Column(String, unique=True, index=True)
+    token = Column(String(255), unique=True, index=True)
 
     user = relationship("User", back_populates="tokens")
     
